4fa01aa60714496063eb9c257f346b8b260f5edc
Abhishek Choudhary <abhi@Abhisheks-MacBook-Pro.local>
2021-12-03 21:55:14 +0530

    index.js, nodemon

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-const express = require('express')
-const app = express()
- 
+
+  const express = require ("express")
+  const app = express()
+  const port = 3000
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World, Hey Youtube')
 })
+
+app.listen(port, ()=>{console.log("My project is running ...")})
+
+// function displayMessage(){
+//   console.log("My project is running ...")
+// }
+
+
+
+
+
+
+// const express = require('express')
+// const app = express()
  
-app.listen(3000)
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+ 
+// app.listen(3000)
