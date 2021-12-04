190860dc5a6419686cadaa241af41a8ae96210a5
Abhishek Choudhary <abhi@Abhisheks-MacBook-Pro.local>
2021-12-04 21:32:57 +0530

    index.js added apis

--- index.js
+++ index.js
@@ -3,8 +3,25 @@
   const app = express()
   const port = 3000
 
+// get apis
 app.get('/', function (req, res) {
-  res.send('Hello World, Hey Youtube')
+  res.send('Hello World')
+})
+app.get('/login/', function (req, res) {
+  res.send('Welcome to login page')
+})
+app.get('/admin/', function (req, res) {
+  res.send('Welcome to admin page')
+})
+// post apis
+app.post('/', function (req, res) {
+  res.send('Hello World post')
+})
+app.post('/login/', function (req, res) {
+  res.send('Welcome to login page post')
+})
+app.post('/admin/', function (req, res) {
+  res.send('Welcome to admin page post')
 })
 
 app.listen(port, ()=>{console.log("My project is running ...")})
